1e0fbc96bfd38f63e7aed3dc11a96432a91b72b7
athenas2021 <lucas.kano@outlook.com>
2022-04-08 08:51:08 -0300

    tests..2

--- notepadauto.py
+++ notepadauto.py
@@ -1,12 +1,25 @@
 from pywinauto.application import Application
 from pywinauto.findwindows import find_elements
+from pywinauto.findwindows import find_windows
 import time
 
 # https://pywinauto.readthedocs.io/en/latest/HowTo.html
 # https://github.com/pywinauto/pywinauto
 
 # Listar elementos abertos
-print(find_elements())
+for element in find_elements():
+    print(element)
+    #print(dir(element))
+    print('name',element.name)
+    print('process_id',element.process_id)
+    print('wm_get_ctrl_name',element.wm_get_ctrl_name)
+    print('dump_window',element.dump_window)
+    print('control_id',element.control_id)
+    print('automation_id',element.automation_id)
+
+for window in find_windows():
+    print('window', window)
+    print('dir->',dir(window))
 
 # Listar processos
 
@@ -16,19 +29,32 @@ print(find_elements())
 # app.UntitledNotepad.menu_select("Help->About Notepad")
 # app.AboutNotepad.OK.click()
 # app.UntitledNotepad.Edit.type_keys("pywinauto Works!", with_spaces = True)
+#opera2 = None
 try:
+    print('Abrindo...')
     notepad = Application().connect(title_re=".*Bloco de Notas", class_name="Notepad")
     notepad.UntitledNotepad.Edit.type_keys("o pitoco nao presta!", with_spaces = True)
+    print('notepad ok')
+    app = Application().connect(path=r"C:\Users\lucas\AppData\Local\Programs\Opera\opera.exe")
+    print('app opera path ok')
+    opera2 = Application().connect(title_re=".*Opera.*", class_name="Opera")
+    print('opera 2 ok')
+    dialogs = app.windows()
+    # for win in dialogs:
+    #     print(dialogs)
 except :
     print(f'Algo deu errado - ')
-opera2 = Application().connect(title_re=".*Opera Internet Browser", class_name="Opera")
-app = Application().connect(path=r"C:/Users/lucas/AppData/Local/Programs/Opera/opera.exe")
-print('Abrindo...')
-dialogs = app.windows()
-for win in dialogs:
-    print(dialogs)
-time.sleep(4)
-opera =  app.window(title_re = ".*Opera.*")
+
+time.sleep(2)
+opera =  Application().connect(process = 9008)
+#print(opera)
+#print(dir(opera))
 print('OK')
-print((opera.MenuItems()))
-# print(dir(opera.Properties.OK))
+print(((app.window().print_control_identifiers())))
+print(((app.window().actions)))
+
+for window in app.windows():
+    print(window)
+    print(window.window_text)
+    print(window.texts)
+    print(window.menu_items)
